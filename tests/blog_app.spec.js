ea8a7ede09255967b4ca4a4962f9f76c687a6e46
hamesomer11 <hamzaomer45@gmail.com>
2024-09-01 22:06:59 +0100

    Blog List End-to-End Testing: Exercise 5.18

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,7 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'rootUser',
+        username: 'root',
+        password: 'root'
+      }
+    })
+
     await page.goto('http://localhost:5173')
   })
 
@@ -10,5 +19,26 @@ describe('Blog app', () => {
     await expect(page.getByTestId('username')).toBeVisible()
     await expect(page.getByTestId('password')).toBeVisible()
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('root')
+      await page.getByTestId('password').fill('root')
+      await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('rootUser logged-in')).toBeVisible()
+    })
+
+    test('login fails with wrong password', async ({ page }) =>{
+      await page.getByTestId('username').fill('wrong')
+      await page.getByTestId('password').fill('wrong')
+      await page.getByRole('button', { name: 'login' }).click()
+    
+      const errorDiv = page.locator('.error')
+      await expect(errorDiv).toContainText('Wrong Username or Password')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+      await expect(page.getByText('rootUser logged-in')).not.toBeVisible()
+    })
   })
 })
