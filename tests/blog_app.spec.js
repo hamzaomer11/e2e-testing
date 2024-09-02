cc71bc40c3cd19b2bbfcd020658a796f8d71de80
hamesomer11 <hamzaomer45@gmail.com>
2024-09-02 14:01:55 +0100

    Blog List End-to-End Testing: Exercise 5.19

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+import { loginWith } from './helper'
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -23,22 +24,34 @@ describe('Blog app', () => {
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-      await page.getByTestId('username').fill('root')
-      await page.getByTestId('password').fill('root')
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, 'root', 'root')
       await expect(page.getByText('rootUser logged-in')).toBeVisible()
     })
 
     test('login fails with wrong password', async ({ page }) =>{
-      await page.getByTestId('username').fill('wrong')
-      await page.getByTestId('password').fill('wrong')
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, 'wrong', 'wrong')
     
       const errorDiv = page.locator('.error')
       await expect(errorDiv).toContainText('Wrong Username or Password')
       await expect(errorDiv).toHaveCSS('border-style', 'solid')
       await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
       await expect(page.getByText('rootUser logged-in')).not.toBeVisible()
+    })
+  })
+
+  describe('when logged in', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, 'root', 'root')
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'new blog' }).click()
+      await page.getByTestId('title').fill('test-blog')
+      await page.getByTestId('author').fill('test-author')
+      await page.getByTestId('url').fill('www.newblog.com')
+      await page.getByRole('button', { name: 'create' }).click()
+      await expect(page.getByText('a new blog test-blog by test-author added')).toBeVisible()
+      await expect(page.getByText('test-blog test-author')).toBeVisible()
     })
   })
 })
